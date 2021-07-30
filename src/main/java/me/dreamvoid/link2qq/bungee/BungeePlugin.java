226d2dbe2b6732ed97984340059abc01ae1d3616
DreamVoid <45266046+DreamVoid@users.noreply.github.com>
2021-07-30 13:57:12 +0800

    支持BungeeCord

---
+++ src/main/java/me/dreamvoid/link2qq/bungee/BungeePlugin.java
@@ -0,0 +1,106 @@
+package me.dreamvoid.link2qq.bungee;
+
+import me.dreamvoid.link2qq.bungee.commands.link2qq;
+import me.dreamvoid.link2qq.internal.Config;
+import me.dreamvoid.link2qq.internal.Utils;
+import me.dreamvoid.miraimc.api.MiraiBot;
+import me.dreamvoid.miraimc.api.MiraiMC;
+import me.dreamvoid.miraimc.bukkit.event.MiraiFriendMessageEvent;
+import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+
+import java.util.Arrays;
+
+public class BungeePlugin extends Plugin implements Listener {
+    @Override
+    public void onLoad() {
+        new Config(this);
+    }
+
+    @Override
+    public void onEnable() {
+        Config.loadConfigBungee();
+        ProxyServer.getInstance().getPluginManager().registerCommand(this,new link2qq(this,"link2qq"));
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @EventHandler
+    public void onFriendMessageReceive(MiraiFriendMessageEvent e){
+        if(e.getBotID() == Config.Bot_Id){
+            getProxy().getScheduler().runAsync(this, () -> {
+                String[] args = e.getMessage().split(" ");
+                if(args[0].equals(Config.Bot_AddBindCommand)){
+                    if(args.length >= 2){
+                        Utils.qqBind.remove(e.getSenderID());
+                        Utils.qqCode.remove(e.getSenderID());
+                        Utils.qqBind.put(e.getSenderID(),args[1]);
+                        Utils.qqCode.put(e.getSenderID(),Utils.getRandomString(Config.Bot_ConfirmCodeLength));
+                        for (String s : Arrays.asList("请在游戏内输入指令以完成绑定流程：", "/link2qq verify " + e.getSenderID() + " " + Utils.qqCode.get(e.getSenderID()))) {
+                            MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage(s);
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException interruptedException) {
+                                interruptedException.printStackTrace();
+                            }
+                        }
+                    } else MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("参数不足，请检查消息内容！");
+                }
+                if(args[0].equals(Config.Bot_ConfirmBindCommand)){
+                    if(args.length >= 3){
+                        String name = args[1];
+                        String code = args[2];
+                        if(getProxy().getPlayer(name) != null && Utils.playerBind.get(name) != null && Utils.playerCode.get(name) != null && Utils.playerBind.get(name) == (e.getSenderID()) && Utils.playerCode.get(name).equals(code)){
+                            MiraiMC.addBinding(getProxy().getPlayer(name).getUniqueId().toString(), e.getSenderID());
+                            MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("已成功添加绑定！如需更换绑定，请直接发起新的绑定；如需取消绑定，请联系管理员！");
+                        } else MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("无法核对您的信息，请检查您的输入或重新发起绑定！");
+                    } else MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("参数不足，请检查消息内容！");
+                }
+            });
+        }
+    }
+
+    @EventHandler
+    public void onGroupMessageReceive(MiraiGroupMessageEvent e){
+        if(e.getBotID() == Config.Bot_Id && e.getGroupID() == Config.Bot_Group){
+            getProxy().getScheduler().runAsync(this, () -> {
+                String[] args = e.getMessage().split(" ");
+                if(args[0].equals(Config.Bot_AddBindCommand)){
+                    if(args.length >= 2){
+                        Utils.qqBind.remove(e.getSenderID());
+                        Utils.qqCode.remove(e.getSenderID());
+                        Utils.qqBind.put(e.getSenderID(),args[1]);
+                        Utils.qqCode.put(e.getSenderID(),Utils.getRandomString(Config.Bot_ConfirmCodeLength));
+                        for (String s : Arrays.asList("请在游戏内输入指令以完成绑定流程：", "/link2qq verify " + e.getSenderID() + " " + Utils.qqCode.get(e.getSenderID()))) {
+                            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage(s);
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException interruptedException) {
+                                interruptedException.printStackTrace();
+                            }
+                        }
+                    } else MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("参数不足，请检查消息内容！");
+                }
+                if(args[0].equals(Config.Bot_ConfirmBindCommand)){
+                    if(args.length >= 3){
+                        String name = args[1];
+                        String code = args[2];
+                        if(getProxy().getPlayer(name) != null && Utils.playerBind.get(name) != null && Utils.playerCode.get(name) != null && Utils.playerBind.get(name) == (e.getSenderID()) && Utils.playerCode.get(name).equals(code)){
+                            MiraiMC.addBinding(getProxy().getPlayer(name).getUniqueId().toString(), e.getSenderID());
+                            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("已成功添加绑定！如需更换绑定，请直接发起新的绑定；如需取消绑定，请联系管理员！");
+                            Utils.playerBind.remove(name);
+                            Utils.playerCode.remove(name);
+                        } else MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("无法核对您的信息，请检查您的输入或重新发起绑定！");
+                    } else MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("参数不足，请检查消息内容！");
+                }
+            });
+        }
+    }
+
+}
--- src/main/java/me/dreamvoid/link2qq/internal/Config.java
+++ src/main/java/me/dreamvoid/link2qq/internal/Config.java
@@ -1,11 +1,19 @@
 package me.dreamvoid.link2qq.internal;
 
 import me.dreamvoid.link2qq.bukkit.BukkitPlugin;
+import me.dreamvoid.link2qq.bungee.BungeePlugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class Config {
     private BukkitPlugin plugin;
+    private static BungeePlugin BungeePlugin;
+    private static Configuration bungeeConfig;
 
     public static long Bot_Id;
     public static long Bot_Group;
@@ -15,6 +23,9 @@ public class Config {
 
     public Config(BukkitPlugin plugin){
         this.plugin = plugin;
+    }
+    public Config(BungeePlugin bungee){
+        BungeePlugin = bungee;
     }
 
     public void loadConfig(){
@@ -27,5 +38,30 @@ public class Config {
         Bot_AddBindCommand = plugin.getConfig().getString("bot.add-bind-command","添加绑定");
         Bot_ConfirmBindCommand = plugin.getConfig().getString("bot.confirm-bind-command","确认绑定");
         Bot_ConfirmCodeLength = plugin.getConfig().getInt("bot.confirm-code-length", 6);
+    }
+
+    public static void loadConfigBungee(){
+        try {
+            if (!BungeePlugin.getDataFolder().exists()) {
+                if(!BungeePlugin.getDataFolder().mkdir()) throw new IOException();
+            }
+            File file = new File(BungeePlugin.getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                try (InputStream in = BungeePlugin.getResourceAsStream("config.yml")) {
+                    Files.copy(in, file.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            bungeeConfig = ConfigurationProvider.getProvider(net.md_5.bungee.config.YamlConfiguration.class).load(new File(BungeePlugin.getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bot_Id = bungeeConfig.getLong("bot.bot-account");
+        Bot_Group = bungeeConfig.getLong("bot.group-id");
+        Bot_AddBindCommand = bungeeConfig.getString("bot.add-bind-command","添加绑定");
+        Bot_ConfirmBindCommand = bungeeConfig.getString("bot.confirm-bind-command","确认绑定");
+        Bot_ConfirmCodeLength = bungeeConfig.getInt("bot.confirm-code-length", 6);
     }
 }
